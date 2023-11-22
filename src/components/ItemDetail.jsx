390bced6c94b0d4deedf62c2200a475315daef6e
AllanNara <allannara@outlook.com>
2023-11-22 04:31:27 -0300

    fix button 'finalizar compra'

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -9,14 +9,14 @@ function ItemDetail({ item }) {
 	const [quantityAdded, setQuantity] = useState(0);
 
 	const handleOnAdd = (quantity) => {
-		setQuantity(quantity)
-	}
+		setQuantity(quantity);
+	};
 
 	return (
 		<>
 			<div
 				style={{
-          display: "flex",
+					display: "flex",
 					flexDirection: "column",
 					justifyContent: "center",
 					alignItems: "center",
@@ -26,34 +26,31 @@ function ItemDetail({ item }) {
 					border: "1px solid black",
 					marginTop: 7,
 				}}
-        >
-        <Link to={"/"}>
-					<button style={{alignSelf: "flex-start"}}>{'<'} Volver al listado</button>
+			>
+				<Link to={"/"}>
+					<button style={{ alignSelf: "flex-start" }}>{"<"} Volver al listado</button>
 				</Link>
 				<img src={image} alt={title} style={{ height: "150px", width: "150px" }} />
 				<h2>{title}</h2>
 				<span>Categoria: {category}</span>
 				<span>Precio: {price}</span>
 				<p>{description}</p>
-				{quantityAdded > 0 ? 
-				(<Button
-					variant="outlined"
-					sx={{ width: 230, p: 1 }}
-				>
-					<Link to={"/cart"} style={{ color: 'inherit', textDecoration: 'inherit'}}>
-						<Typography
-							textTransform="initial"
-							variant="body1"
-							fontWeight={200}
-							fontSize={13}
-						>
-							Finalizar compra
-						</Typography>
+				{quantityAdded > 0 ? (
+					<Link to={"/cart"} style={{ color: "inherit", textDecoration: "inherit" }}>
+						<Button variant="outlined" sx={{ width: 230, p: 1 }}>
+							<Typography
+								textTransform="initial"
+								variant="body1"
+								fontWeight={200}
+								fontSize={13}
+							>
+								Finalizar compra
+							</Typography>
+						</Button>
 					</Link>
-				</Button>
-				)
-				: (<ItemCount initial={1} stock={10} onAdd={handleOnAdd}/>) 
-			}
+				) : (
+					<ItemCount initial={1} stock={10} onAdd={handleOnAdd} />
+				)}
 			</div>
 		</>
 	);
